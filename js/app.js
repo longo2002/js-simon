5b99c8e18f22d67f1ff763104c2d5078d7ce4184
longo2002 <lombardo.fra2002@gmail.com>
2023-02-13 18:24:07 +0100

    done

---
+++ js/app.js
@@ -0,0 +1,40 @@
+console.log("works!")
+
+let btnEl = document.getElementById("btn")
+const nums = []
+const rightNums = []
+const wrongNums = []
+
+function numsGenerator() {
+    do {
+        const randomNumber = Math
+            .floor(Math.random() * 100) + 1
+        nums.push(randomNumber);
+    } while (nums.length < 5);
+}
+
+function userNumsGatherer() {
+
+    for (let i = 0; i < 5; i++) {
+        let inputNums = parseInt(prompt("Inserisci i numeri precedenti"))
+        if (nums.includes(inputNums)) {
+            rightNums.push(inputNums)
+        } else {
+            wrongNums.push(inputNums)
+        }
+    }
+}
+
+btnEl.addEventListener("click",
+    function runEl() {
+        numsGenerator()
+        alert(nums)
+        setTimeout(userNumsGatherer, 3000) 
+    }
+)
+
+
+console.log(nums, rightNums, wrongNums)
+
+
+
